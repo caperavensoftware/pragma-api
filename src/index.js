70a92b29dda596f760c4c34764a33248c62aa1bc
Malcolm Jack <inquisitorjax@gmail.com>
2017-06-01 17:33:20 +0200

    fix file path

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-export * from './api/onkey-api.js';
+// note that this path is relevant to the paths that are determined in the publish.js file (hence no api folder)
+export * from './onkey-api.js';
